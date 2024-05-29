06aad98f6f14bc1ca81c962444bb7d38e4e32a90
SIXIVESS <mettatonbb@gmail.com>
2024-05-28 20:10:26 -0700

    Javascript

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let app = document.getElementById('typewriter');
- 
+
 let typewriter = new Typewriter(app, {
   loop: true,
-  delay: 75,
+  delay: 75, 
 });
- 
+
 typewriter
-  .pauseFor(2500)
+  .pauseFor(2500) 
   .typeString('SIXIVESS')
-  .pauseFor(200)
+  .pauseFor(4000) 
   .deleteChars(10)
   .start();
